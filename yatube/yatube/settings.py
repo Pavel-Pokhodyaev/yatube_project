56b69b028db264fc17e5c06951d5f79bfee84eed
Pavel-Pokhodyaev <pavka_91@mail.ru>
2022-03-04 17:18:50 +0300

    GoGoGo

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ice_cream.apps.IceCreamConfig',
+    'post.apps.Post.Comfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
